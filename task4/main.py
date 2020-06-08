8452e17b32566076321c960e2f381260ce97228b
Agil89 <agil.makhmudov@mail.ru>
2020-06-08 17:49:20 +0400

    task4

---
+++ task4/main.py
@@ -0,0 +1,26 @@
+# Write a Python program to add year(s) with a given date and display the new date.
+#
+# Sample Data : (addYears is the user defined function name)
+# print(addYears(datetime.date(2015,1,1), -1))
+# print(addYears(datetime.date(2015,1,1), 0))
+# print(addYears(datetime.date(2015,1,1), 2))
+# print(addYears(datetime.date(2000,2,29),1))
+#
+# Expected Output :
+# 2014-01-01
+# 2015-01-01
+# 2017-01-01
+# 2001-03-01
+from datetime import *
+import datetime
+from datetime import date
+
+def addYears(x,y):
+   print(x+timedelta(y*365))
+
+
+
+addYears(datetime.date(2015,1,1), -1)
+addYears(datetime.date(2015,1,1), 0)
+addYears(datetime.date(2015,1,1), 2)
+addYears(datetime.date(2000,2,29),1)
